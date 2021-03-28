e230c9b8be2c060aa98b1209f2584f70f0cc6ec9
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-03-28 04:21:53 -0300

    chore: install and configure eslint and prettier

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,20 @@
 module.exports = function (api) {
-    api.cache(true);
-    const presets = ['@babel/preset-env', '@babel/preset-typescript'];
-    const plugins = [
-        [require.resolve('babel-plugin-module-resolver'),
-        {
-            alias: {
-                '@entities': './src/entities'
-            }
-        }]
-    ]
+  api.cache(false);
+  const presets = ['@babel/preset-env', '@babel/preset-typescript'];
+  const plugins = [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['.'],
+        alias: {
+          '@entities': './src/entities',
+        },
+      },
+    ],
+  ];
 
-    return {
-        plugins,
-        presets
-    };
-}
+  return {
+    plugins,
+    presets,
+  };
+};
