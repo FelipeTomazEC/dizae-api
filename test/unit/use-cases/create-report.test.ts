62e3136219131c9e1029befeae0df24b444e651b
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-04-18 14:01:43 -0300

    test: add tests for the create report use case

---
+++ test/unit/use-cases/create-report.test.ts
@@ -0,0 +1,140 @@
+import { Item } from '@entities/location/item/item';
+import { Location } from '@entities/location/location';
+import { Report } from '@entities/report/report';
+import { Status } from '@entities/report/status';
+import { Reporter } from '@entities/reporter/reporter';
+import { Id } from '@entities/shared/id/id';
+import { getMock } from '@test/test-helpers/get-mock';
+import { CreateReportUseCase } from '@use-cases/create-report/create-report';
+import { CreateReportRequest } from '@use-cases/create-report/dtos/create-report-request';
+import { CreateReportResponse } from '@use-cases/create-report/dtos/create-report-response';
+import { IdGenerator } from '@use-cases/interfaces/adapters/id-generator';
+import { UseCaseOutputPort } from '@use-cases/interfaces/ports/use-case-output-port';
+import { LocationRepository } from '@use-cases/interfaces/repositories/location';
+import { ReportRepository } from '@use-cases/interfaces/repositories/report';
+import { ReporterRepository } from '@use-cases/interfaces/repositories/reporter';
+import { ResourceNotFoundError } from '@use-cases/shared/errors/resource-not-found-error';
+import * as faker from 'faker';
+
+describe('Create report use case tests.', () => {
+  const reportRepository = getMock<ReportRepository>(['save']);
+  const locationRepository = getMock<LocationRepository>(['getLocationById']);
+  const reporterRepository = getMock<ReporterRepository>(['getReporterById']);
+  const idGenerator = getMock<IdGenerator>(['generate']);
+  const presenter = getMock<UseCaseOutputPort<CreateReportResponse>>([
+    'failure',
+    'success',
+  ]);
+
+  const sut = new CreateReportUseCase({
+    idGenerator,
+    locationRepository,
+    presenter,
+    reporterRepository,
+    reportRepository,
+  });
+
+  let item: Item;
+  let reporter: Reporter;
+  let location: Location;
+  let id: Id;
+
+  const request: CreateReportRequest = {
+    description: faker.lorem.sentence(5),
+    image: faker.image.image(),
+    itemName: faker.commerce.product(),
+    locationId: faker.random.uuid(),
+    reporterId: faker.random.uuid(),
+    title: faker.lorem.words(2),
+  };
+
+  beforeAll(() => {
+    reporter = Reporter.create({
+      name: 'Test User',
+      createdAt: Date.now(),
+      avatar: faker.internet.avatar(),
+      email: faker.internet.email(),
+      id: request.reporterId,
+      password: 'som3Pa$$word1221',
+    }).value as Reporter;
+
+    location = Location.create({
+      name: faker.commerce.department(),
+      creatorId: faker.random.uuid(),
+      id: request.locationId,
+      createdAt: Date.now(),
+    }).value as Location;
+
+    item = Item.create({
+      categoryId: faker.random.uuid(),
+      createdAt: Date.now(),
+      creatorId: faker.random.uuid(),
+      image: faker.image.image(),
+      name: request.itemName,
+    }).value as Item;
+
+    id = Id.create({ value: faker.random.uuid() }).value as Id;
+
+    location.addItem(item);
+
+    jest
+      .spyOn(locationRepository, 'getLocationById')
+      .mockResolvedValue(location);
+    jest
+      .spyOn(reporterRepository, 'getReporterById')
+      .mockResolvedValue(reporter);
+    jest.spyOn(idGenerator, 'generate').mockReturnValue(id);
+    jest.spyOn(Date, 'now').mockReturnValue(Date.now());
+  });
+
+  it('should verify if the reporter exists.', async () => {
+    jest
+      .spyOn(reporterRepository, 'getReporterById')
+      .mockResolvedValueOnce(undefined);
+
+    await sut.execute(request);
+
+    expect(presenter.failure).toBeCalledWith(
+      new ResourceNotFoundError('reporter'),
+    );
+  });
+
+  it('should verify if the location exists.', async () => {
+    jest
+      .spyOn(locationRepository, 'getLocationById')
+      .mockResolvedValueOnce(undefined);
+
+    await sut.execute(request);
+
+    expect(presenter.failure).toBeCalledWith(
+      new ResourceNotFoundError('location'),
+    );
+  });
+
+  it('should verify if the item exists in the location.', async () => {
+    await sut.execute({ ...request, itemName: 'Not Registered Item' });
+
+    expect(presenter.failure).toBeCalledWith(new ResourceNotFoundError('item'));
+  });
+
+  it('should save the new report in the repository and return its id.', async () => {
+    const report = Report.create({
+      createdAt: Date.now(),
+      creatorId: reporter.id.value,
+      description: request.description,
+      id: id.value,
+      image: request.image,
+      itemLocationId: location.id.value,
+      itemName: item.name.value,
+      status: Status.PENDING,
+      title: request.title,
+    }).value as Report;
+
+    await sut.execute(request);
+
+    expect(reportRepository.save).toBeCalledWith(report);
+    expect(presenter.success).toBeCalledWith({
+      newReportId: id.value,
+    });
+  });
+});
