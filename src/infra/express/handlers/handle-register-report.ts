2494f6f7c33de1d6c321843e02a7acb5f29ed0af
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-04-30 07:23:08 -0300

    feat: create register reporter express handler

---
+++ src/infra/express/handlers/handle-register-report.ts
@@ -0,0 +1,37 @@
+import { ErrorLogger } from '@interface-adapters/controllers/interfaces/error-logger';
+import { RegisterReporterController } from '@interface-adapters/controllers/register-reporter';
+import { HttpStatusCode } from '@interface-adapters/http/http-status-code';
+import { IdGenerator } from '@use-cases/interfaces/adapters/id-generator';
+import { PasswordEncoder } from '@use-cases/interfaces/adapters/password-encoder';
+import { ReporterRepository } from '@use-cases/interfaces/repositories/reporter';
+import { RegisterReporterUseCase } from '@use-cases/register-reporter/register-reporter';
+import { Request, Response } from 'express';
+import { createHttpPresenter } from '../helpers/create-http-presenter';
+import { parseToHttpRequest } from '../helpers/parse-to-http-request';
+
+interface Dependencies {
+  repository: ReporterRepository;
+  logger: ErrorLogger;
+  idGenerator: IdGenerator;
+  encoder: PasswordEncoder;
+}
+
+export const handleRegisterReporter = (deps: Dependencies) => (
+  req: Request,
+  res: Response,
+): void => {
+  const presenter = createHttpPresenter(res, HttpStatusCode.RESOURCE_CREATED);
+  const useCase = new RegisterReporterUseCase({
+    encoder: deps.encoder,
+    idGenerator: deps.idGenerator,
+    presenter,
+    repository: deps.repository,
+  });
+  const controller = new RegisterReporterController(
+    deps.logger,
+    useCase,
+    presenter,
+  );
+
+  controller.handle(parseToHttpRequest(req));
+};
