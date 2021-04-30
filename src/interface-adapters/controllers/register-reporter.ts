4dd1b1770f1b3cf32098f5a2e9c9c116d2ad4f8a
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-04-29 21:14:18 -0300

    feat: create register reporter controller

---
+++ src/interface-adapters/controllers/register-reporter.ts
@@ -0,0 +1,11 @@
+import { HttpRequest } from '@interface-adapters/http/http-request';
+import { RegisterReporterRequest as Request } from '@use-cases/register-reporter/dtos/register-reporter-request';
+import { WebController } from './web-controller';
+
+export class RegisterReporterController extends WebController<Request> {
+  getParamsFromHttpRequest(request: HttpRequest): Request {
+    const { avatar, email, name, password } = request.body;
+
+    return { avatar, email, name, password };
+  }
+}
