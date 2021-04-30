bd0cb9b4c39c456036f09c4f3231634f1236d0bd
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-04-30 07:30:19 -0300

    feat: create express application

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+/* eslint-disable no-console */
+import { setupMiddlewares } from '@infra/express/helpers/setup-middlewares';
+import { setupRoutes } from '@infra/express/helpers/setup-routes';
+import express from 'express';
+
+const PORT = process.env.PORT ?? 3000;
+
+(async () => {
+  try {
+    const application = express();
+
+    setupMiddlewares(application);
+    setupRoutes(application);
+
+    application.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}...`);
+    });
+  } catch (err) {
+    console.error('Occurred an error while starting the application.', err);
+  }
+})();
