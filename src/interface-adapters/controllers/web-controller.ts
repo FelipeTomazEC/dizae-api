46910aa5543368c88b12c8dae05c00a6efb1bca3
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-04-29 21:13:09 -0300

    feat: create base web controller

---
+++ src/interface-adapters/controllers/web-controller.ts
@@ -0,0 +1,26 @@
+import { HttpRequest } from '@interface-adapters/http/http-request';
+import { UseCaseInputPort } from '@use-cases/interfaces/ports/use-case-input-port';
+import { UseCaseOutputPort } from '@use-cases/interfaces/ports/use-case-output-port';
+import { InternalServerError } from './errors/internal-server-error';
+import { ErrorLogger } from './interfaces/error-logger';
+import { HttpController } from './interfaces/http-controller';
+
+export abstract class WebController<T> implements HttpController {
+  constructor(
+    private readonly errorLogger: ErrorLogger,
+    private readonly useCase: UseCaseInputPort<T>,
+    private readonly presenter: UseCaseOutputPort<any>,
+  ) {}
+
+  async handle(request: HttpRequest): Promise<void> {
+    try {
+      const useCaseRequest = this.getParamsFromHttpRequest(request);
+      return await this.useCase.execute(useCaseRequest);
+    } catch (err) {
+      await this.errorLogger.log(err);
+      return this.presenter.failure(new InternalServerError());
+    }
+  }
+
+  abstract getParamsFromHttpRequest(request: HttpRequest): T;
+}
