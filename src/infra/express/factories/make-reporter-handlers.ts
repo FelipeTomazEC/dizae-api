ca1f55b2263e9864479d3716bbf180e8d6d5f767
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-04-30 07:24:42 -0300

    feat: implement factory to create all reporter handlers

---
+++ src/infra/express/factories/make-reporter-handlers.ts
@@ -0,0 +1,23 @@
+import { InMemoryReporterRepository } from '@infra/database/repositories/in-memory-reporter-repository';
+import { BcryptPasswordEncoder } from '@infra/implementations/bcrypt-password-encoder';
+import { ConsoleErrorLogger } from '@infra/implementations/console-error-logger';
+import { UUIDV4Generator } from '@infra/implementations/uuid-v4-generator';
+import { handleRegisterReporter } from '../handlers/handle-register-report';
+
+export const makeReporterHandlers = () => {
+  const repository = new InMemoryReporterRepository();
+  const idGenerator = new UUIDV4Generator();
+  const encoder = new BcryptPasswordEncoder();
+  const logger = new ConsoleErrorLogger();
+
+  const registerReporterHandler = handleRegisterReporter({
+    repository,
+    encoder,
+    idGenerator,
+    logger,
+  });
+
+  return {
+    registerReporterHandler,
+  };
+};
