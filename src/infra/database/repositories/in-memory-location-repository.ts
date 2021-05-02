0139dd07c7ae69ff79e7998afcf5ea4c3897e568
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-05-02 10:48:22 -0300

    feat: create in memory location repository

---
+++ src/infra/database/repositories/in-memory-location-repository.ts
@@ -0,0 +1,36 @@
+import { Location } from '@entities/location/location';
+import { Id } from '@entities/shared/id/id';
+import { Name } from '@entities/shared/name/name';
+import { LocationRepository } from '@use-cases/interfaces/repositories/location';
+
+export class InMemoryLocationRepository implements LocationRepository {
+  private static instance: InMemoryLocationRepository | null = null;
+
+  private readonly records: Location[];
+
+  private constructor() {
+    this.records = [];
+  }
+
+  static getInstance(): InMemoryLocationRepository {
+    if (this.instance === null) {
+      this.instance = new InMemoryLocationRepository();
+    }
+    return this.instance;
+  }
+
+  getLocationById(id: Id): Promise<Location | undefined> {
+    const location = this.records.find((r) => r.id.isEqual(id));
+    return Promise.resolve(location);
+  }
+
+  exists(name: Name): Promise<boolean> {
+    const exists = this.records.some((r) => r.name.isEqual(name));
+    return Promise.resolve(exists);
+  }
+
+  save(location: Location): Promise<void> {
+    this.records.push(location);
+    return Promise.resolve();
+  }
+}
