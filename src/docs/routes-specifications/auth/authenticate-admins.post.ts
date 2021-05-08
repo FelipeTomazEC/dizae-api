7f8f18ee66c32fe1b477c8d88bbef5aac471a8b1
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-05-08 18:05:48 -0300

    docs: create authenticate admin route documentation

---
+++ src/docs/routes-specifications/auth/authenticate-admins.post.ts
@@ -0,0 +1,129 @@
+const requestSchema = {
+  type: 'object',
+  required: ['email', 'password'],
+  properties: {
+    email: {
+      type: 'string',
+      description: `The admin's e-mail.`,
+    },
+    password: {
+      type: 'string',
+      description: `The admin's password.`,
+    },
+  },
+  example: {
+    email: 'admin@example.com',
+    password: 'Some pas$w0rd',
+  },
+};
+
+const responseSchema = {
+  type: 'object',
+  properties: {
+    success: {
+      type: 'boolean',
+      description: 'Flag that indicates wether the login is successful or not.',
+    },
+    data: {
+      type: 'object',
+      properties: {
+        credentials: {
+          type: 'string',
+          description:
+            'A json web token that can be used in protected admin routes.',
+        },
+        expiresIn: {
+          type: 'number',
+          description: 'The expiration time of the token in seconds.',
+        },
+      },
+    },
+  },
+  example: {
+    success: true,
+    data: {
+      credentials: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ8.eyJvd25lcklkIjp7InByb3BzIjp7InZhbHVlIjoiMjlkNDJlZmItYzAyNy00MzAyLTg1MzUtNDQwNmRiNTFmNGRkIn19LCJpYXQiOjE2MTk5MDAzNjUsImV4cCI6MTYxOTkwMzk2NX0.5MQqTW3RaBhndGd-E7s2MtCvUxge_IAHIi_B5-7GlBc`,
+      expiresIn: 3600,
+    },
+  },
+};
+
+const errorSchema = {
+  type: 'object',
+  properties: {
+    success: {
+      type: 'boolean',
+      description: 'Flag that indicates wether the admin was register or not.',
+    },
+    error: {
+      type: 'object',
+      properties: {
+        message: {
+          type: 'string',
+          description: 'A message that tells what was wrong.',
+        },
+      },
+    },
+  },
+  example: {
+    success: false,
+    error: {
+      message: 'Specific error message.',
+    },
+  },
+};
+
+export default {
+  post: {
+    tags: ['Auth'],
+    summary: 'Authenticate the admin in the application.',
+
+    requestBody: {
+      description: 'The expected params for authenticate an admin.',
+      required: true,
+      content: {
+        'application/json': {
+          schema: requestSchema,
+        },
+      },
+    },
+
+    responses: {
+      200: {
+        description: 'Returns a token for the admin.',
+        content: {
+          'application/json': {
+            schema: responseSchema,
+          },
+        },
+      },
+
+      400: {
+        description: 'Returns an error informing that the e-mail is not valid.',
+        content: {
+          'application/json': {
+            schema: errorSchema,
+          },
+        },
+      },
+
+      401: {
+        description: 'Returns an error informing that the password is wrong.',
+        content: {
+          'application/json': {
+            schema: errorSchema,
+          },
+        },
+      },
+
+      404: {
+        description: 'Inform that the given e-mail is not registered.',
+        content: {
+          'application/json': {
+            schema: errorSchema,
+          },
+        },
+      },
+    },
+  },
+};
--- src/docs/routes-specifications/auth/index.ts
+++ src/docs/routes-specifications/auth/index.ts
@@ -1,5 +1,7 @@
 import authenticateReportersSpecification from './authenticate-reporters.post';
+import authenticateAdminsSpecification from './authenticate-admins.post';
 
 export const authPaths = {
+  '/auth/admins': authenticateAdminsSpecification,
   '/auth/reporters': authenticateReportersSpecification,
 };
