89ecff7d098b2167eeeb42a98da458d66bc3810e
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-05-11 21:24:29 -0300

    feat: integrate item categories routes to the api

---
+++ src/infra/main/make-item-categories-handler.ts
@@ -0,0 +1,24 @@
+import { InMemoryAdminRepository } from '@infra/database/repositories/in-memory-admin-repository';
+import { InMemoryItemCategoryRepository } from '@infra/database/repositories/in-memory-item-category-repository';
+import { getCreateItemCategoryHandler } from '@infra/express/handlers/get-create-item-category-handler';
+import { ItemCategoriesHandler } from '@infra/express/routers/get-item-categories-router';
+import { ConsoleErrorLogger } from '@infra/implementations/console-error-logger';
+import { JWTAuthService } from '@infra/implementations/jwt-auth-service';
+
+export const makeItemCategoriesHandler = (): ItemCategoriesHandler => {
+  const adminRepo = InMemoryAdminRepository.getInstance();
+  const authorizer = new JWTAuthService(process.env.ADMINS_JWT_SECRET!);
+  const itemCategoryRepo = InMemoryItemCategoryRepository.getInstance();
+  const logger = new ConsoleErrorLogger();
+
+  const handleCreateItemCategory = getCreateItemCategoryHandler({
+    adminRepo,
+    authorizer,
+    itemCategoryRepo,
+    logger,
+  });
+
+  return {
+    handleCreateItemCategory,
+  };
+};
--- src/infra/main/setup-routes.ts
+++ src/infra/main/setup-routes.ts
@@ -1,10 +1,12 @@
 import { getAdminsRouter } from '@infra/express/routers/get-admins-router';
+import { getItemCategoriesRouter } from '@infra/express/routers/get-item-categories-router';
 import { getLocationRouter } from '@infra/express/routers/get-location-router';
 import { Express } from 'express';
 import { getAuthRouter } from '../express/routers/get-auth-router';
 import { getReportersRouter } from '../express/routers/get-reporters-router';
 import { makeAdminsHandler } from './make-admins-handler';
 import { makeAuthHandler } from './make-auth-handler';
+import { makeItemCategoriesHandler } from './make-item-categories-handler';
 import { makeLocationsHandler } from './make-locations-handler';
 import { makeReportersHandler } from './make-reporters-handler';
 
@@ -13,4 +15,5 @@ export const setupRoutes = (app: Express): void => {
   app.use(getAuthRouter(makeAuthHandler()));
   app.use(getAdminsRouter(makeAdminsHandler()));
   app.use(getLocationRouter(makeLocationsHandler()));
+  app.use(getItemCategoriesRouter(makeItemCategoriesHandler()));
 };
