e9e4da570d8a7b7c2738716869105483e4872a39
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-05-12 22:04:21 -0300

    feat: configure add item to location route in the api

--- src/infra/main/make-locations-handler.ts
+++ src/infra/main/make-locations-handler.ts
@@ -1,5 +1,7 @@
 import { InMemoryAdminRepository } from '@infra/database/repositories/in-memory-admin-repository';
+import { InMemoryItemCategoryRepository } from '@infra/database/repositories/in-memory-item-category-repository';
 import { InMemoryLocationRepository } from '@infra/database/repositories/in-memory-location-repository';
+import { createAddItemToLocationHandler } from '@infra/express/handlers/create-add-item-to-location-handler';
 import { getCreateLocationHandler } from '@infra/express/handlers/get-create-location-handler';
 import { LocationsHandler } from '@infra/express/routers/get-location-router';
 import { ConsoleErrorLogger } from '@infra/implementations/console-error-logger';
@@ -9,6 +11,7 @@ import { UUIDV4Generator } from '@infra/implementations/uuid-v4-generator';
 export const makeLocationsHandler = (): LocationsHandler => {
   const adminRepo = InMemoryAdminRepository.getInstance();
   const locationRepo = InMemoryLocationRepository.getInstance();
+  const categoryRepo = InMemoryItemCategoryRepository.getInstance();
   const authorizer = new JWTAuthService(process.env.ADMINS_JWT_SECRET!);
   const logger = new ConsoleErrorLogger();
   const idGenerator = new UUIDV4Generator();
@@ -21,5 +24,13 @@ export const makeLocationsHandler = (): LocationsHandler => {
     logger,
   });
 
-  return { handleCreateLocation };
+  const handleAddItemToLocation = createAddItemToLocationHandler({
+    adminRepo,
+    authorizer,
+    categoryRepo,
+    locationRepo,
+    logger,
+  });
+
+  return { handleCreateLocation, handleAddItemToLocation };
 };
