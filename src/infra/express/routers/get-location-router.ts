2320035f217214990f2f77f0bd1e316873ce7ec6
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-05-12 22:02:51 -0300

    feat: create route to add item to location

--- src/infra/express/routers/get-location-router.ts
+++ src/infra/express/routers/get-location-router.ts
@@ -3,12 +3,14 @@ import { ExpressHandleFunction } from './express-handle-function.type';
 
 export interface LocationsHandler {
   handleCreateLocation: ExpressHandleFunction;
+  handleAddItemToLocation: ExpressHandleFunction;
 }
 
 export const getLocationRouter = (handler: LocationsHandler): Router => {
   const router = Router();
 
   router.post('/locations', handler.handleCreateLocation);
+  router.post('/locations/:locationId/items', handler.handleAddItemToLocation);
 
   return router;
 };
