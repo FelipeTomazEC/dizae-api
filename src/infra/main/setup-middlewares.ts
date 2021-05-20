6c84bcd7b691e1b3ff7c9ecbd9f393150befa0d7
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-05-19 22:26:15 -0300

    config: add cors middleware

--- src/infra/main/setup-middlewares.ts
+++ src/infra/main/setup-middlewares.ts
@@ -1,5 +1,7 @@
 import { Express, json } from 'express';
+import cors from 'cors';
 
 export const setupMiddlewares = (application: Express) => {
+  application.use(cors());
   application.use(json());
 };
