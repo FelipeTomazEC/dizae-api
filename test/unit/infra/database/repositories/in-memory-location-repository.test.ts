2ba69568cba3d1b5bbfb2fb2fcda0616e39737e2
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-05-20 23:40:25 -0300

    test: implement get all method

--- test/unit/infra/database/repositories/in-memory-location-repository.test.ts
+++ test/unit/infra/database/repositories/in-memory-location-repository.test.ts
@@ -1,3 +1,4 @@
+import { Item } from '@entities/location/item/item';
 import { Location } from '@entities/location/location';
 import { Id } from '@entities/shared/id/id';
 import { Name } from '@entities/shared/name/name';
@@ -46,5 +47,47 @@ describe('In memory location repository tests.', () => {
     const instance2 = InMemoryLocationRepository.getInstance();
 
     expect(instance1).toEqual(instance2);
+  });
+
+  it('should return all locations, include their items.', async () => {
+    const createLocation = () => {
+      const loc = Location.create({
+        createdAt: Date.now(),
+        creatorId: faker.datatype.uuid(),
+        id: faker.datatype.uuid(),
+        name: faker.commerce.department(),
+      }).value as Location;
+
+      const item = Item.create({
+        categoryName: faker.commerce.productMaterial(),
+        createdAt: Date.now(),
+        creatorId: faker.datatype.uuid(),
+        image: faker.image.image(),
+        name: faker.commerce.product(),
+      }).value as Item;
+
+      loc.addItem(item);
+
+      return loc;
+    };
+
+    const location1 = createLocation();
+    const location2 = createLocation();
+    const location3 = createLocation();
+
+    const alreadySaved = await sut.getAll();
+
+    await Promise.all([
+      sut.save(location1),
+      sut.save(location2),
+      sut.save(location3),
+    ]);
+
+    const retrieved = await sut.getAll();
+
+    expect(retrieved.length).toBe(alreadySaved.length + 3);
+    expect(retrieved).toContain(location1);
+    expect(retrieved).toContain(location2);
+    expect(retrieved).toContain(location3);
   });
 });
