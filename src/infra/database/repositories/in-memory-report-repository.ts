83b4ba7ec0b55494e32cb5d98eea1423f019f752
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-05-24 20:44:26 -0300

    feat: implement method get all

--- src/infra/database/repositories/in-memory-report-repository.ts
+++ src/infra/database/repositories/in-memory-report-repository.ts
@@ -1,5 +1,9 @@
 import { Report } from '@entities/report/report';
-import { ReportRepository } from '@use-cases/interfaces/repositories/report';
+import {
+  GetReportsFilters,
+  ReportRepository,
+} from '@use-cases/interfaces/repositories/report';
+import { Pagination } from '@use-cases/shared/pagination-settings';
 
 export class InMemoryReportRepository implements ReportRepository {
   private static instance: InMemoryReportRepository | null = null;
@@ -22,5 +26,23 @@ export class InMemoryReportRepository implements ReportRepository {
     const key = report.id.value;
     this.records.set(key, report);
     return Promise.resolve();
+  }
+
+  getAll(
+    filters: GetReportsFilters,
+    pagination: Pagination,
+  ): Promise<Report[]> {
+    const { status, since, locationsIds } = filters;
+    const result = Array.from(this.records.values())
+      .sort((r1, r2) => r1.createdAt - r2.createdAt)
+      .filter(
+        (r) =>
+          !locationsIds ||
+          locationsIds.some((l) => r.item.locationId.value === l),
+      )
+      .filter((r) => !status || status.some((s) => r.status === s))
+      .filter((r) => !since || r.createdAt >= since);
+
+    return Promise.resolve(result.splice(pagination.start, pagination.offset));
   }
 }
