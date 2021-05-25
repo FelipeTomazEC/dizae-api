d16a9bf96ce48c39d85d12a70703f44e04d31fb8
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-05-25 13:15:07 -0300

    feat: add get reports route in the API

--- src/infra/express/routers/get-report-router.ts
+++ src/infra/express/routers/get-report-router.ts
@@ -3,12 +3,14 @@ import { ExpressHandleFunction } from './express-handle-function.type';
 
 export interface ReportsHandler {
   handleCreateReport: ExpressHandleFunction;
+  handleGetReports: ExpressHandleFunction;
 }
 
 export const getReportRouter = (handler: ReportsHandler): Router => {
   const router = Router();
 
   router.post('/reports', handler.handleCreateReport);
+  router.get('/reports', handler.handleGetReports);
 
   return router;
 };
--- src/infra/main/make-reports-handler.ts
+++ src/infra/main/make-reports-handler.ts
@@ -1,22 +1,29 @@
+import { InMemoryAdminRepository } from '@infra/database/repositories/in-memory-admin-repository';
 import { InMemoryLocationRepository } from '@infra/database/repositories/in-memory-location-repository';
 import { InMemoryReportRepository } from '@infra/database/repositories/in-memory-report-repository';
 import { InMemoryReporterRepository } from '@infra/database/repositories/in-memory-reporter-repository';
+import { createGetReportsHandler } from '@infra/express/handlers/create-get-reports-handler';
 import { createReportHandler } from '@infra/express/handlers/get-create-report-handler';
 import { ReportsHandler } from '@infra/express/routers/get-report-router';
+import { AuthorizerComposer } from '@infra/implementations/authorizer-composer';
 import { ConsoleErrorLogger } from '@infra/implementations/console-error-logger';
 import { JWTAuthService } from '@infra/implementations/jwt-auth-service';
 import { UUIDV4Generator } from '@infra/implementations/uuid-v4-generator';
 
 export const makeReportsHandler = (): ReportsHandler => {
-  const authorizer = new JWTAuthService(process.env.REPORTERS_JWT_SECRET!);
+  const reporterAuthorizer = new JWTAuthService(
+    process.env.REPORTERS_JWT_SECRET!,
+  );
+  const adminAuthorizer = new JWTAuthService(process.env.ADMINS_JWT_SECRET!);
   const locationRepo = InMemoryLocationRepository.getInstance();
   const idGenerator = new UUIDV4Generator();
   const reporterRepo = InMemoryReporterRepository.getInstance();
   const reportRepo = InMemoryReportRepository.getInstance();
+  const adminRepo = InMemoryAdminRepository.getInstance();
   const logger = new ConsoleErrorLogger();
 
   const handleCreateReport = createReportHandler({
-    authorizer,
+    authorizer: reporterAuthorizer,
     idGenerator,
     locationRepo,
     logger,
@@ -24,5 +31,14 @@ export const makeReportsHandler = (): ReportsHandler => {
     reporterRepo,
   });
 
-  return { handleCreateReport };
+  const handleGetReports = createGetReportsHandler({
+    adminRepo,
+    authorizer: new AuthorizerComposer(adminAuthorizer, reporterAuthorizer),
+    locationRepo,
+    logger,
+    reportRepo,
+    reporterRepo,
+  });
+
+  return { handleCreateReport, handleGetReports };
 };
