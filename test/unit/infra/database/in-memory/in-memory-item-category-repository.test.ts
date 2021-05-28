90a99549c8a94781a98b902d38f9be8fe3542d0e
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-05-28 08:03:45 -0300

    refactor: rewrite using common repository tests

--- test/unit/infra/database/in-memory/in-memory-item-category-repository.test.ts
+++ test/unit/infra/database/in-memory/in-memory-item-category-repository.test.ts
@@ -1,53 +1,25 @@
-import { ItemCategory } from '@entities/item-category/item-category';
+import { Admin } from '@entities/admin/admin';
 import { InMemoryItemCategoryRepository } from '@infra/database/in-memory/in-memory-item-category-repository';
-
 import faker from 'faker';
+import { itemCategoryRepositoryTests } from '../common/item-category-repository-tests';
 
 describe('In memory item category repository tests.', () => {
   const sut = InMemoryItemCategoryRepository.getInstance();
-  const itemCategory = ItemCategory.create({
+  const admin = Admin.create({
+    avatar: faker.image.avatar(),
     createdAt: Date.now(),
-    creatorId: faker.datatype.uuid(),
-    name: faker.commerce.productMaterial(),
-  }).value as ItemCategory;
+    email: faker.internet.email(),
+    id: faker.datatype.uuid(),
+    name: 'Admin Tests',
+    password: 'Some3PA$$word',
+  }).value as Admin;
 
-  it('should save an item category.', async () => {
-    await expect(sut.save(itemCategory)).resolves.toBeFalsy();
-  });
-
-  it('should return true if an item category exists, false otherwise.', async () => {
-    await sut.save(itemCategory);
-    const exists = await sut.exists(itemCategory.name);
-
-    expect(exists).toBe(true);
-  });
+  itemCategoryRepositoryTests(sut, admin);
 
   it('should be a singleton.', async () => {
     const instance1 = InMemoryItemCategoryRepository.getInstance();
     const instance2 = InMemoryItemCategoryRepository.getInstance();
 
     expect(instance1).toBe(instance2);
-  });
-
-  it('should return all categories saved.', async () => {
-    const createItemCategory = () =>
-      ItemCategory.create({
-        createdAt: Date.now(),
-        creatorId: faker.datatype.uuid(),
-        name: faker.commerce.productMaterial(),
-      }).value as ItemCategory;
-
-    const categoriesBeforeSaving = await sut.getAll();
-    const cat1 = createItemCategory();
-    const cat2 = createItemCategory();
-    await sut.save(cat1);
-    await sut.save(cat2);
-    const categoriesAfterSaving = await sut.getAll();
-
-    expect(categoriesAfterSaving).toContain(cat1);
-    expect(categoriesAfterSaving).toContain(cat2);
-    expect(categoriesAfterSaving.length).toBe(
-      categoriesBeforeSaving.length + 2,
-    );
   });
 });
