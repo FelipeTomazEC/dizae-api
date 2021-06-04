837674765320270b82473678a778335c18635b96
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-06-04 19:49:15 -0300

    chore: modify knex production settings

--- knexfile.js
+++ knexfile.js
@@ -32,12 +32,8 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      ssl: { rejectUnauthorized: false },
+      connectionString: process.env.DATABASE_URL,
+      ssl: {rejectUnauthorized: false}
     },
     pool: {
       min: 2,
