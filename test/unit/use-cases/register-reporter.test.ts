d79bf3eacb4bcc33d914aae08a94ebf0bfb1486c
FelipeTomazEC <felipetomaz2206@gmail.com>
2021-07-08 22:56:58 -0300

    test: add avatar upload step in register reporter use case

--- test/unit/use-cases/register-reporter.test.ts
+++ test/unit/use-cases/register-reporter.test.ts
@@ -6,6 +6,7 @@ import { Id } from '@entities/shared/id/id';
 import { Password } from '@entities/shared/password/password';
 import { left } from '@shared/either.type';
 import { IdGenerator } from '@use-cases/interfaces/adapters/id-generator';
+import { ImageUploadService } from '@use-cases/interfaces/adapters/image-upload-service';
 import { PasswordEncoder } from '@use-cases/interfaces/adapters/password-encoder';
 import { UseCaseOutputPort } from '@use-cases/interfaces/ports/use-case-output-port';
 import { ReporterRepository } from '@use-cases/interfaces/repositories/reporter';
@@ -20,25 +21,30 @@ describe('Register Reporter use case tests.', () => {
   const encoder = getMock<PasswordEncoder>(['encode']);
   const idGenerator = getMock<IdGenerator>(['generate']);
   const presenter = getMock<UseCaseOutputPort<any>>(['failure', 'success']);
-
-  beforeAll(() => {
-    const id = Id.create({ value: faker.datatype.uuid() }).value as Id;
-    jest.spyOn(idGenerator, 'generate').mockReturnValue(id);
-  });
+  const imageUploadService = getMock<ImageUploadService>(['upload']);
 
   const sut = new RegisterReporterUseCase({
     repository,
     encoder,
     idGenerator,
     presenter,
+    imageUploadService,
   });
 
   const request: RegisterReporterRequest = {
     email: faker.internet.email(),
     password: 'SomePassword15@',
-    avatar: faker.internet.avatar(),
+    avatar: 'some-base-64-image',
     name: faker.name.findName(),
   };
+
+  const avatarURL = faker.image.imageUrl();
+  const id = Id.create({ value: faker.datatype.uuid() }).value as Id;
+
+  beforeAll(() => {
+    jest.spyOn(idGenerator, 'generate').mockReturnValue(id);
+    jest.spyOn(imageUploadService, 'upload').mockResolvedValue(avatarURL);
+  });
 
   it('should register only an unregistered email.', async () => {
     jest.spyOn(repository, 'emailExists').mockResolvedValueOnce(true);
@@ -70,12 +76,18 @@ describe('Register Reporter use case tests.', () => {
     expect(presenter.failure).toBeCalledWith(error);
   });
 
-  it(`should return the new reporter's id.`, async () => {
-    const id = Id.create({ value: faker.datatype.uuid() }).value as Id;
-    jest.spyOn(idGenerator, 'generate').mockReturnValueOnce(id);
-
+  it('should upload the avatar using the image upload service', async () => {
     await sut.execute(request);
 
-    expect(presenter.success).toBeCalledWith({ reporterId: id.value });
+    expect(imageUploadService.upload).toBeCalledWith(request.avatar, id.value);
+  });
+
+  it(`should return the new reporter's id and their avatar url.`, async () => {
+    await sut.execute(request);
+
+    expect(presenter.success).toBeCalledWith({
+      reporterId: id.value,
+      avatar: avatarURL,
+    });
   });
 });
